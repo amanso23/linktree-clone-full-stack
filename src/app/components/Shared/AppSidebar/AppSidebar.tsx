776a57280bf0058b7d6fd4c902bcf07636f306df
Adrian <adrianmanso80@gmail.com>
2025-03-10 16:35:16 +0000

    Add AppSidebar component for navigation with menu items

---
+++ src/app/components/Shared/AppSidebar/AppSidebar.tsx
@@ -0,0 +1,76 @@
+import {
+  AlignVerticalSpaceAround,
+  CalendarRange,
+  ChartNoAxesColumnIncreasing,
+  Eclipse,
+  Settings,
+  Store,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Links",
+    url: "#",
+    icon: AlignVerticalSpaceAround,
+  },
+  {
+    title: "Shop",
+    url: "#",
+    icon: Store,
+  },
+  {
+    title: "Appearence",
+    url: "#",
+    icon: Eclipse,
+  },
+  {
+    title: "Social Planner",
+    url: "#",
+    icon: CalendarRange,
+  },
+  {
+    title: "Analytics",
+    url: "#",
+    icon: ChartNoAxesColumnIncreasing,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
