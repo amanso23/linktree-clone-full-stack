1b4985f0579dc4fa7e3baa8922860083cfe0f749
Adrian <adrianmanso80@gmail.com>
2025-03-10 13:36:47 +0000

    Add API route for user information retrieval and creation on first login

---
+++ src/app/api/info-user/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { prisma } from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    let existingUser = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      include: { links: true },
+    });
+
+    if (!existingUser) {
+      existingUser = await prisma.user.create({
+        data: {
+          id: userId,
+          name: "User",
+          username: `user_${Date.now()}`,
+          links: {
+            create: [],
+          },
+        },
+        include: { links: true },
+      });
+    }
+
+    return NextResponse.json(existingUser);
+  } catch (err) {
+    console.error("[GET_USER_FIRST_LOGIN]", err);
+    return NextResponse.json(
+      {
+        message: "Error fetching user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
