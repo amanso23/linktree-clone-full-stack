9a61d2624a5345be9cbe8d001c60b4cbd469e9b1
Adrian <adrianmanso80@gmail.com>
2025-03-10 19:41:42 +0000

    Add dataCreator array for StepOne component options

---
+++ src/app/(routes)/(home)/components/StepOne/data.ts
@@ -0,0 +1,22 @@
+export const dataCreator = [
+  {
+    title: "✨ Creator",
+    value: "creator",
+  },
+  {
+    title: "🏢 Business",
+    value: "business",
+  },
+  {
+    title: "💻 Developer",
+    value: "developer",
+  },
+  {
+    title: "🏠 Real State",
+    value: "real-state",
+  },
+  {
+    title: "😊 Other",
+    value: "other",
+  },
+];
