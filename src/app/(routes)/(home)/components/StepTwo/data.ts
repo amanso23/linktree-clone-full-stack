75de06d69ef6b38fc6a5c50d359c570917830f7a
Adrian <adrianmanso80@gmail.com>
2025-03-10 19:41:52 +0000

    Add stepTwoData array for StepTwo component options

---
+++ src/app/(routes)/(home)/components/StepTwo/data.ts
@@ -0,0 +1,42 @@
+export const stepTwoData = [
+  {
+    icon: "https://626a6b3cns.ufs.sh/f/cQaenfiARKuVXwVYMbbhNHd2c76OVS3xqIgfeouipl4GFZPj",
+    name: "Youtube",
+    link: "",
+  },
+  {
+    icon: "https://626a6b3cns.ufs.sh/f/cQaenfiARKuVoPCSJjcS8X9tr0wnm7xD3kQJG6AliMeCb1js",
+    name: "Instagram",
+    link: "",
+  },
+  {
+    icon: "https://626a6b3cns.ufs.sh/f/cQaenfiARKuVuQJIzi80K9gQD1fxm74G5HWnjUi86MNhJeAu",
+    name: "Facebook",
+    link: "",
+  },
+  {
+    icon: "https://626a6b3cns.ufs.sh/f/cQaenfiARKuV90E4uJXnCyW5slzBd18c3HRqoLt42SNbMeOh",
+    name: "Dribble",
+    link: "",
+  },
+  {
+    icon: "https://626a6b3cns.ufs.sh/f/cQaenfiARKuVrfH7nrqKVkO2fyeY1hQWJ8TacPim5pMvw0H7",
+    name: "Twitter X",
+    link: "",
+  },
+  {
+    icon: "https://626a6b3cns.ufs.sh/f/cQaenfiARKuVkMQzwzpqFgZDwS8ylxNBXO7nPHJce9MVC5U0",
+    name: "Linkedin",
+    link: "",
+  },
+  {
+    icon: "https://626a6b3cns.ufs.sh/f/cQaenfiARKuVP7rt0JVD5KdwY4n8OmxFbA03Z2uGkCSfMzEo",
+    name: "TikTok",
+    link: "",
+  },
+  {
+    icon: "https://626a6b3cns.ufs.sh/f/cQaenfiARKuVVS2eL5tjlKtMWuCrR2NSi31I0E5UFafc4Gwd",
+    name: "Pinterest",
+    link: "",
+  },
+];
