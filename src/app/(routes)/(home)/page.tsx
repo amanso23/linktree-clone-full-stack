ccc1428cd7dc38ace7ee51f91a1db9f88eb26bb1
Adrian <adrianmanso80@gmail.com>
2025-03-10 11:02:14 +0000

    Enhance Home component: add Profile section and layout for improved user experience

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,3 +1,25 @@
+import { TreePalm } from "lucide-react";
+import Profile from "./components/Profile/Profile";
+
 export default function Home({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-[60%_auto]">
+        <section className="grid">
+          <Profile />
+          <h1>Profile info</h1>
+          <div className="grid place-items-center opacity-60">
+            <TreePalm className="w-auto h-16 " />
+            <h2>Show the world who you are.</h2>
+            <h3>Add a link to get started</h3>
+          </div>
+        </section>
+
+        <section>
+          <h1>Profile preview</h1>
+        </section>
+      </div>
+      {children}
+    </div>
+  );
 }
