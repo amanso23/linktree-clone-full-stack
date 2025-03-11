72c8ec4c3f197cc98f4a7fbac709b83a9177c767
Adrian <adrianmanso80@gmail.com>
2025-03-11 12:22:07 +0000

    Enhance StepTwo component to manage platform selection and update user info

--- src/app/(routes)/(home)/components/StepTwo/StepTwo.tsx
+++ src/app/(routes)/(home)/components/StepTwo/StepTwo.tsx
@@ -2,21 +2,52 @@ import { Button } from "@/components/ui/button";
 import { useStepConfig } from "@/hooks/useStepConfig";
 import { stepTwoData } from "./data";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { InfoUserType } from "@/contexts/StepConfigUser/types";
+import { platform } from "os";
 
 export function StepTwo() {
   const { setInfoUser, infoUser, nextStep } = useStepConfig();
-  const [selectedPlatforms, setSelectedPlatforms] = useState([]);
+  const [selectedPlatforms, setSelectedPlatforms] = useState<
+    InfoUserType["platforms"]
+  >([]);
+
+  console.log(infoUser);
+
+  const selectedClass = "bg-black text-white hover:opacity-85";
+
+  const handleClick = (icon: string, name: string, link: string) => {
+    setSelectedPlatforms((prevState) => {
+      if (prevState.some((state) => state.name === name)) {
+        return prevState.filter((s) => s.name !== name);
+      }
+      return [...prevState, { icon, name, link }];
+    });
+  };
+
+  useEffect(() => {
+    setInfoUser((prevState) => {
+      if (prevState.platforms !== selectedPlatforms) {
+        return { ...prevState, platforms: selectedPlatforms };
+      }
+      return prevState;
+    });
+  }, [selectedPlatforms]);
 
   return (
     <section className="text-center">
       <h2 className="text-2xl">Which platforms are you</h2>
       <p>Pickup the ones you are on.</p>
       <ul className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
-        {stepTwoData.map(({ name, icon }) => (
+        {stepTwoData.map(({ name, icon, link }) => (
           <li
             key={name}
-            className="flex flex-col gap-1 items-center justify-center rounded-lg border py-3 transition-all duration-300 hover:bg-gray-200 cursor-pointer"
+            className={`flex flex-col gap-1 items-center justify-center rounded-lg border py-3 transition-all duration-300 cursor-pointer ${
+              selectedPlatforms.some((platform) => platform.name === name)
+                ? selectedClass
+                : "hover:bg-gray-100"
+            }`}
+            onClick={() => handleClick(icon, name, link)}
           >
             <Image src={icon} alt="Icon" width={40} height={40} />
             <p className="text-sm">{name}</p>
@@ -24,7 +55,7 @@ export function StepTwo() {
         ))}
       </ul>
 
-      <Button className="mt-4">Continue</Button>
+      <Button className="mt-4 w-full">Continue</Button>
     </section>
   );
 }
