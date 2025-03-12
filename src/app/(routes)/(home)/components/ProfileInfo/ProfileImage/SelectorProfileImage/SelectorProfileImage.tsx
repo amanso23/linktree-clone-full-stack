7fd77a5b510a2f8578747da8fb02458f17050c8e
Adrian <adrianmanso80@gmail.com>
2025-03-12 16:25:28 +0000

    Add SelectorProfileImage component for managing profile image selection

---
+++ src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/SelectorProfileImage/SelectorProfileImage.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { SelectorProfileImageProps } from "./types";
+import { TabSelector } from "./TabSelector";
+import { TabUpload } from "./TabUpload";
+
+export function SelectorProfileImage({
+  setShowDialog,
+}: SelectorProfileImageProps) {
+  const [showTab, setShowTab] = useState<"upload" | "delete" | null>(null);
+
+  return (
+    <div className="mt-2">
+      {!showTab && <TabSelector setShowTab={setShowTab} />}
+      {showTab === "upload" && (
+        <TabUpload setShowDialog={setShowDialog} setShowTab={setShowTab} />
+      )}
+      {showTab === "delete" && <p>Tab delete image</p>}
+    </div>
+  );
+}
