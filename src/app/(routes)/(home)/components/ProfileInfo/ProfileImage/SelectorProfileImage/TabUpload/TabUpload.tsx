31958ee7ac9b8ba2ce46d8fb48b0a907e67f3bc7
Adrian <adrianmanso80@gmail.com>
2025-03-12 16:25:18 +0000

    Add TabUpload component for avatar image upload functionality

---
+++ src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/SelectorProfileImage/TabUpload/TabUpload.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { TabUploadProps } from "./types";
+import { ChevronLeft } from "lucide-react";
+import { UploadButton } from "@/lib/uploadthing";
+import { Button } from "@/components/ui/button";
+import { useUserInfo } from "@/hooks/useUser";
+import Image from "next/image";
+import axios from "axios";
+
+export function TabUpload({ setShowDialog, setShowTab }: TabUploadProps) {
+  const { user, reloadUser } = useUserInfo();
+  const [avatarUrl, setAvatarUrl] = useState("");
+
+  const onUploadAvatar = async () => {
+    const response = await axios.patch("api/update-user", {
+      avatarUrl: avatarUrl,
+    });
+
+    if (response.status === 200) {
+      setShowDialog(false);
+      reloadUser();
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div
+        className="flex gap-1 items-center text-sm cursor-pointer hover:bg-slate-100 p-2 w-fit rounded-lg"
+        onClick={() => setShowTab(null)}
+      >
+        <ChevronLeft className="w-4 h-4" />
+        Back
+      </div>
+
+      {!avatarUrl ? (
+        <UploadButton
+          className="rounded-md text-slate-800 bg-slate-200 w-full h-full p-4"
+          endpoint={"profileImage"}
+          onClientUploadComplete={(res) => {
+            setAvatarUrl(res?.[0].ufsUrl);
+          }}
+          onUploadError={(error: Error) => {
+            console.error(error);
+          }}
+        />
+      ) : (
+        <Image
+          src={avatarUrl}
+          alt="ProfileImage"
+          width={300}
+          height={300}
+          className="w-20 h-auto rounded-full self-center"
+        />
+      )}
+
+      <Button className="w-full" onClick={onUploadAvatar}>
+        {" "}
+        Upload
+      </Button>
+    </div>
+  );
+}
