c23fb7dfe7eea2e1755b109eb2360465097f9152
Adrian <adrianmanso80@gmail.com>
2025-03-12 16:24:42 +0000

    Add API route to update user avatar

---
+++ src/app/api/update-user/route.ts
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(req: NextRequest) {
+  try {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
+      return NextResponse.json("Unauthorized", { status: 401 });
+    }
+
+    const data = await req.json();
+
+    const { avatarUrl } = data;
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        avatarUrl,
+      },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error updating avatar", error: error },
+      { status: 500 }
+    );
+  }
+}
