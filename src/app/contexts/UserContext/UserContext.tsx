9fbf9a371a5b0d9978c47abee3bbbc1b5c9ac1de
Adrian <adrianmanso80@gmail.com>
2025-03-12 16:25:00 +0000

    Add UserContext and useUserInfo hook for user state management

---
+++ src/app/contexts/UserContext/UserContext.tsx
@@ -0,0 +1,51 @@
+import { createContext, useEffect, useState } from "react";
+import { UserContextType, UserProviderProps } from "./types";
+import { useUser } from "@clerk/nextjs";
+import { Link, User } from "@prisma/client";
+
+export const UserContext = createContext<UserContextType>({
+  user: null,
+  isLoading: false,
+  links: null,
+  reloadUser: () => {},
+});
+
+export function UserProvider({ children }: UserProviderProps) {
+  const { user } = useUser();
+  const [infoUser, setInfoUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [links, setLinks] = useState<Link[] | null>(null);
+
+  const fetchUserInfo = async () => {
+    if (!user) return;
+
+    try {
+      setIsLoading(true);
+      const response = await fetch("api/info-user");
+      const data = await response.json();
+      setInfoUser(data);
+      setLinks(data.links || []);
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, [user]);
+
+  const reloadUser = () => {
+    fetchUserInfo();
+  };
+
+  const data = {
+    user: infoUser,
+    isLoading,
+    links,
+    reloadUser,
+  };
+
+  return <UserContext.Provider value={data}>{children}</UserContext.Provider>;
+}
