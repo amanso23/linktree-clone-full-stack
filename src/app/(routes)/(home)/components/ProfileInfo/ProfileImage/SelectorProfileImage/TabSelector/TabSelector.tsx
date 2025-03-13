e65609ef829672a45a5ca9b74cedf5bdaa67d05c
Adrian <adrianmanso80@gmail.com>
2025-03-13 10:37:50 +0000

    Conditionally render delete option in TabSelector based on user avatar presence

--- src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/SelectorProfileImage/TabSelector/TabSelector.tsx
+++ src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/SelectorProfileImage/TabSelector/TabSelector.tsx
@@ -1,7 +1,10 @@
-import { ChevronRight, Delete, ImageUp, Trash2 } from "lucide-react";
+import { ChevronRight, ImageUp, Trash2 } from "lucide-react";
 import { TabSelectorProps } from "./types";
+import { useUserInfo } from "@/hooks/useUser";
 
 export function TabSelector({ setShowTab }: TabSelectorProps) {
+  const { user } = useUserInfo();
+
   return (
     <ul className="flex flex-col gap-4 items-center cursor-pointer text-black">
       <li
@@ -20,19 +23,21 @@ export function TabSelector({ setShowTab }: TabSelectorProps) {
         <ChevronRight className="h-4 w-4" />
       </li>
 
-      <li
-        className="flex justify-between items-center gap-4 rounded-lg p-2 w-full h-fit hover:bg-gray-100"
-        onClick={() => setShowTab("delete")}
-      >
-        <div className="flex items-center gap-4">
-          <Trash2 className="opacity-70 text-red-500" strokeWidth={1} />
-          <div className="flex flex-col">
-            <h2 className="block font-semibold">Delete</h2>
-            <h4 className="text-xs opacity-70">Delete current image</h4>
+      {user?.avatarUrl && (
+        <li
+          className="flex justify-between items-center gap-4 rounded-lg p-2 w-full h-fit hover:bg-gray-100"
+          onClick={() => setShowTab("delete")}
+        >
+          <div className="flex items-center gap-4">
+            <Trash2 className="opacity-70 text-red-500" strokeWidth={1} />
+            <div className="flex flex-col">
+              <h2 className="block font-semibold">Delete</h2>
+              <h4 className="text-xs opacity-70">Delete current image</h4>
+            </div>
           </div>
-        </div>
-        <ChevronRight className="h-4 w-4" />
-      </li>
+          <ChevronRight className="h-4 w-4" />
+        </li>
+      )}
     </ul>
   );
 }
