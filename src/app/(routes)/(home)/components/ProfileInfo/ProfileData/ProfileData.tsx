be2933c855f0429dd1c8737e25f6b81691d95d78
Adrian <adrianmanso80@gmail.com>
2025-03-13 17:02:25 +0000

    Improve ProfileData component to handle missing user information gracefully

--- src/app/(routes)/(home)/components/ProfileInfo/ProfileData/ProfileData.tsx
+++ src/app/(routes)/(home)/components/ProfileInfo/ProfileData/ProfileData.tsx
@@ -5,15 +5,31 @@ import { useUserInfo } from "@/hooks/useUser";
 export function ProfileData() {
   const { user } = useUserInfo();
 
-  if (!user) return;
+  if (!user) return null;
 
   const { name, username, bio } = user;
 
   return (
-    <div className="flex flex-col">
-      <h1 className="text-2xl font-bold">{name}</h1>
-      <h3 className="text-sm">@{username}</h3>
-      <p className="text-pretty text-xs opacity-70 max-w-50">{bio}</p>
+    <div className="flex flex-col items-start">
+      {name ? (
+        <h1 className="text-2xl font-bold text-gray-900">{name}</h1>
+      ) : (
+        <h1 className="text-2xl font-semibold text-gray-500">
+          No name available
+        </h1>
+      )}
+
+      {username ? (
+        <h3 className=" text-gray-600 opacity-80">@{username}</h3>
+      ) : (
+        <h3 className=" text-gray-400">No username available</h3>
+      )}
+
+      {bio ? (
+        <p className="text-sm text-gray-700 text-pretty">{bio}</p>
+      ) : (
+        <p className="text-sm text-gray-400">No bio available</p>
+      )}
     </div>
   );
 }
