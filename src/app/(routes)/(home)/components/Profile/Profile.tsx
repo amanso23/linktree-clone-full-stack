32928e94e5a17c623fa08e3418dd25c6d4e668fc
Adrian <adrianmanso80@gmail.com>
2025-03-13 12:17:43 +0000

    Update Profile component to display user-specific link and handle user info loading

--- src/app/(routes)/(home)/components/Profile/Profile.tsx
+++ src/app/(routes)/(home)/components/Profile/Profile.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useUserInfo } from "@/hooks/useUser";
 import { useEffect, useState } from "react";
 
 export function Profile() {
   const [copied, setCopied] = useState(false);
   const [origin, setOrigin] = useState("");
+
+  const { user } = useUserInfo();
+
+  if (!user) return;
 
   useEffect(() => {
     setOrigin(window.location.origin);
@@ -23,7 +28,7 @@ export function Profile() {
       <div className="flex flex-wrap flex-col justify-center text-center py-2 px-4 items-center gap-2 md:flex-row md:justify-start ">
         <h2>✨ Your Linktree clone is live:</h2>
         <span className="link">
-          {origin ? `${origin}/linktreeTest` : "Loading..."}
+          {origin ? `${origin}/${user.username}` : "Loading..."}
         </span>
         <Button
           variant={"outline"}
