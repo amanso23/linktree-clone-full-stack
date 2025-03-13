58c2f53b2777adcda3a272aebf20f0302263c5ab
Adrian <adrianmanso80@gmail.com>
2025-03-13 17:03:06 +0000

    Add EditBackground component to ProfileInfo for background editing functionality

--- src/app/(routes)/(home)/components/ProfileInfo/ProfileInfo.tsx
+++ src/app/(routes)/(home)/components/ProfileInfo/ProfileInfo.tsx
@@ -2,15 +2,18 @@ import { ProfileImage } from "./ProfileImage";
 import { ProfileInfoProps } from "./types";
 import { BlockInfo } from "./ProfileImage/BlockInfo";
 import { ProfileData } from "./ProfileData";
+import { EditBackground } from "./EditBackground";
 
 export function ProfileInfo({ onReload }: ProfileInfoProps) {
   return (
     <section className="flex justify-between items-center p-6">
       <div className="flex items-center gap-4">
         <ProfileImage />
-        <ProfileData />
       </div>
-      <BlockInfo />
+      <div className="flex gap-1">
+        <BlockInfo />
+        <EditBackground onReload={onReload} />
+      </div>
     </section>
   );
 }
