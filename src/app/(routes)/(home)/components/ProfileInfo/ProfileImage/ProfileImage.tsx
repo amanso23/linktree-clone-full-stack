b8c531800775f4136e5d443fa6d3119b303c1db7
Adrian <adrianmanso80@gmail.com>
2025-03-13 12:18:19 +0000

    Refactor ProfileImage component to improve user avatar handling and simplify rendering logic

--- src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/ProfileImage.tsx
+++ src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/ProfileImage.tsx
@@ -13,32 +13,32 @@ import {
 } from "@/components/ui/dialog";
 
 import { SelectorProfileImage } from "./SelectorProfileImage";
+import { Button } from "@/components/ui/button";
 
 export function ProfileImage() {
   const [showDialog, setShowDialog] = useState(false);
   const { user } = useUserInfo();
 
+  if (!user) return;
+
   return (
     <Dialog open={showDialog} onOpenChange={setShowDialog}>
       <DialogTrigger asChild>
-        <div className="cursor-pointer">
-          <div className="relative">
-            <Image
-              src={user?.avatarUrl || "/default-avatar.webp"}
-              alt="Profile Image"
-              width={300}
-              height={300}
-              className="w-20 h-auto aspect-square object-cover rounded-full"
-            />
-
-            <span className="bg-cyan-400 rounded-full size-8 absolute right-0 -bottom-2 flex items-center justify-center border-4 border-gray-100">
-              {user?.avatarUrl ? (
-                <Edit2 className="size-4 text-white" />
-              ) : (
-                <Plus className="size-6 text-white" />
-              )}
-            </span>
-          </div>
+        <div className="relative cursor-pointer">
+          <Image
+            src={user.avatarUrl || "/default-avatar.webp"}
+            alt="Profile Image"
+            width={300}
+            height={300}
+            className="w-20 h-auto aspect-square object-cover rounded-full"
+          />
+          <span className="bg-cyan-400 rounded-full size-8 absolute right-0 -bottom-2 flex items-center justify-center border-4 border-gray-100">
+            {user.avatarUrl ? (
+              <Edit2 className="size-4 text-white" />
+            ) : (
+              <Plus className="size-6 text-white" />
+            )}
+          </span>
         </div>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
