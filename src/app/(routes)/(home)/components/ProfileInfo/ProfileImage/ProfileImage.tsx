39847bd41aecf4ebc92b990446cb1c88b1bd1b24
Adrian <adrianmanso80@gmail.com>
2025-03-13 17:02:07 +0000

    Add tooltip for profile image upload and improve dialog structure

--- src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/ProfileImage.tsx
+++ src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/ProfileImage.tsx
@@ -13,45 +13,64 @@ import {
 } from "@/components/ui/dialog";
 
 import { SelectorProfileImage } from "./SelectorProfileImage";
-import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipArrow,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
 
 export function ProfileImage() {
   const [showDialog, setShowDialog] = useState(false);
   const { user } = useUserInfo();
 
-  if (!user) return;
+  if (!user) return null;
 
   return (
-    <Dialog open={showDialog} onOpenChange={setShowDialog}>
-      <DialogTrigger asChild>
-        <div className="relative cursor-pointer">
-          <Image
-            src={user.avatarUrl || "/default-avatar.webp"}
-            alt="Profile Image"
-            width={300}
-            height={300}
-            className="w-20 h-auto aspect-square object-cover rounded-full"
-          />
-          <span className="bg-cyan-400 rounded-full size-8 absolute right-0 -bottom-2 flex items-center justify-center border-4 border-gray-100">
-            {user.avatarUrl ? (
-              <Edit2 className="size-4 text-white" />
-            ) : (
-              <Plus className="size-6 text-white" />
-            )}
-          </span>
-        </div>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Upload your avatar</DialogTitle>
-          <DialogDescription asChild>
-            <SelectorProfileImage setShowDialog={setShowDialog} />
-          </DialogDescription>
-        </DialogHeader>
-        {/* <DialogFooter>
-          <Button onClick={() => {}}>Save changes</Button>
-        </DialogFooter> */}
-      </DialogContent>
-    </Dialog>
+    <TooltipProvider>
+      <Dialog open={showDialog} onOpenChange={setShowDialog}>
+        <DialogTrigger asChild>
+          <div className="relative cursor-pointer">
+            <Tooltip open={true}>
+              <TooltipTrigger asChild disabled>
+                <Image
+                  src={user.avatarUrl || "/default-avatar.webp"}
+                  alt="Profile Image"
+                  width={300}
+                  height={300}
+                  className="w-20 h-auto aspect-square object-cover rounded-full"
+                />
+              </TooltipTrigger>
+              <TooltipContent
+                align="center"
+                side="right"
+                className="bg-black  text-white p-2 rounded-md shadow-md"
+              >
+                <p className="text-sm">Upload your avatar</p>
+                <TooltipArrow className="fill-black" />
+              </TooltipContent>
+            </Tooltip>
+            <span className="bg-cyan-400 rounded-full size-9 absolute right-0 -bottom-2 flex items-center justify-center border-4 border-gray-100">
+              {user.avatarUrl ? (
+                <Edit2 className="size-4 text-white" />
+              ) : (
+                <Plus className="size-6 text-white" />
+              )}
+            </span>
+          </div>
+        </DialogTrigger>
+
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Upload your avatar</DialogTitle>
+            <DialogDescription>
+              Select an image to set as your new avatar.
+            </DialogDescription>
+          </DialogHeader>
+          <SelectorProfileImage setShowDialog={setShowDialog} />
+        </DialogContent>
+      </Dialog>
+    </TooltipProvider>
   );
 }
