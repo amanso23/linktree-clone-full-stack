40e51d04bd687b24130428b2ed50255994a9cefc
Adrian <adrianmanso80@gmail.com>
2025-03-13 12:17:53 +0000

    Refactor BlockInfo component to remove unused user info handling and simplify state management

--- src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/BlockInfo/BlockInfo.tsx
+++ src/app/(routes)/(home)/components/ProfileInfo/ProfileImage/BlockInfo/BlockInfo.tsx
@@ -1,50 +1,18 @@
-import { useUserInfo } from "@/hooks/useUser";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import axios from "axios";
-import { toast } from "sonner";
+
 import { ProfileForm } from "./ProfileForm";
 
 export function BlockInfo() {
-  const { reloadUser, user } = useUserInfo();
   const [openDialog, setOpenDialog] = useState(false);
-
-  const [name, setName] = useState("");
-  const [username, setUsername] = useState("");
-  const [bio, setBio] = useState("");
-
-  if (!user) return;
-
-  const handleEditProfile = async () => {
-    const response = await axios.patch("/api/update-user", {
-      name,
-      username,
-      bio,
-    });
-    if (response.status === 200) {
-      toast("User Uploaded", {
-        description: "Your user has been successfully uploaded.",
-        duration: 3000,
-        action: {
-          label: "Undo",
-          onClick: () => console.log("Undo clicked"),
-        },
-      });
-      setOpenDialog(false);
-      reloadUser();
-    }
-  };
 
   return (
     <div>
