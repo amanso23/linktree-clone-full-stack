e7d515d39dd8993c5006ab5e1b9986437b5dc93c
Adrian <adrianmanso80@gmail.com>
2025-03-14 14:33:57 +0000

    Add PATCH and DELETE API routes for link management

---
+++ src/app/api/link/[id]/route.ts
@@ -0,0 +1,84 @@
+import { prisma } from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
+      return NextResponse.json(
+        {
+          message: "Unauthorized",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const { id } = await params;
+    const { link } = await req.json();
+
+    const updatedLink = await prisma.link.update({
+      where: {
+        id: id,
+      },
+      data: {
+        link,
+      },
+    });
+
+    return NextResponse.json(updatedLink);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Failed to update the link",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
+      return NextResponse.json({ message: "Unauthorizd" }, { status: 401 });
+    }
+
+    const { id } = await params;
+
+    console.log(id);
+
+    if (!id) {
+      return NextResponse.json({ message: "ID required!" }, { status: 400 });
+    }
+
+    const deletedLink = await prisma.link.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    return NextResponse.json(deletedLink);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Failed to delete the link",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
