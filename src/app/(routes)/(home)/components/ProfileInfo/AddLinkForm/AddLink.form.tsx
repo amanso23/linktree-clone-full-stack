492262c8ab982093a6881b1a4e9315641debf3fe
Adrian <adrianmanso80@gmail.com>
2025-03-14 15:44:00 +0000

    Add AddLinkForm component for link submission with validation

---
+++ src/app/(routes)/(home)/components/ProfileInfo/AddLinkForm/AddLink.form.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { UploadButton } from "@/lib/uploadthing";
+import { AddLinkFormProps } from "./types";
+
+const addLinkFormSchema = z.object({
+  icon: z.string().optional(),
+  link: z
+    .string()
+    .min(2, {
+      message: "Link  must be at least 2 characters.",
+    })
+    .max(200),
+  name: z
+    .string()
+    .min(1, {
+      message: "Name must be at least 2 characters.",
+    })
+    .max(50),
+});
+
+export function AddLinkForm({ onReload }: AddLinkFormProps) {
+  const form = useForm<z.infer<typeof addLinkFormSchema>>({
+    resolver: zodResolver(addLinkFormSchema),
+    defaultValues: {
+      icon: "",
+      link: "",
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof addLinkFormSchema>) => {};
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="icon"
+          render={({ field }) => (
+            <UploadButton
+              endpoint={"profileImage"}
+              onClientUploadComplete={(res) => {}}
+            />
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="link"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Link</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
