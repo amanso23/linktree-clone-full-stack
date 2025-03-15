2d2d8a47ba3959e40225a4e40566142233bc682f
Adrian <adrianmanso80@gmail.com>
2025-03-15 15:29:13 +0000

    Enhance RootLayout component by adding full height to main for improved layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiase`}
         >
-          <main className="w-full">{children}</main>
+          <main className="w-full h-full">{children}</main>
         </body>
       </html>
     </ClerkProvider>
