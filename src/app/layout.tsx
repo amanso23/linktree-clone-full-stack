6afa5b13890c8008e6706c190218e9811c107864
Adrian <adrianmanso80@gmail.com>
2025-03-15 14:58:22 +0000

    Refactor layout to remove minimum height constraint from main element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiase`}
         >
-          <main className="w-full min-h-[calc(100dvh-64px)]">{children}</main>
+          <main className="w-full">{children}</main>
         </body>
       </html>
     </ClerkProvider>
