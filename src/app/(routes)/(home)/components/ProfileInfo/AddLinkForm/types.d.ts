735309b5636b9850622d52b8b441b7d344d26fc5
Adrian <adrianmanso80@gmail.com>
2025-03-15 10:54:20 +0000

    Refactor AddLinkForm types to use interface and extend props for Form component

--- src/app/(routes)/(home)/components/ProfileInfo/AddLinkForm/types.d.ts
+++ src/app/(routes)/(home)/components/ProfileInfo/AddLinkForm/types.d.ts
@@ -1,3 +1,7 @@
-export type AddLinkFormProps = {
+export interface AddLinkFormProps {
   onReload: React.Dispatch<React.SetStateAction<boolean>>;
-};
+}
+
+export interface FormProps extends AddLinkFormProps {
+  setShowDialog: React.Dispatch<React.SetStateAction<boolean>>;
+}
