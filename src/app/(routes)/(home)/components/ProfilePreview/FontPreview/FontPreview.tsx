a3cb343efd35964579438155ebfb13c720d4e7dd
Adrian <adrianmanso80@gmail.com>
2025-03-15 15:19:01 +0000

    Refactor FontPreview component to improve layout and enhance visual elements

--- src/app/(routes)/(home)/components/ProfilePreview/FontPreview/FontPreview.tsx
+++ src/app/(routes)/(home)/components/ProfilePreview/FontPreview/FontPreview.tsx
@@ -1,5 +1,5 @@
 import { useUserInfo } from "@/hooks/useUser";
-import { Palmtree } from "lucide-react";
+import { Palmtree, TreePalm } from "lucide-react";
 import Image from "next/image";
 import { LinksSocial } from "./LinksSocial";
 
@@ -11,7 +11,7 @@ export function FontPreview() {
   const { backgroundImage, avatarUrl, username, bio } = user;
   return (
     <div className="relative mx-auto border-white border-[5px] rounded-[2.5rem] shadow-2xl">
-      <div className="relative rounded-[2rem] overflow-hidden w-[290px] h-[590px]">
+      <div className="relative rounded-[2rem] overflow-hidden w-[290px] h-[590px] overflow-y-auto">
         {backgroundImage ? (
           <Image
             layout="fill"
@@ -32,16 +32,15 @@ export function FontPreview() {
             className="rounded-full object-cover aspect-square"
           />
 
-          <h3 className="font-medium">@{username}</h3>
+          <h3 className="font-medium mt-1">@{username}</h3>
           {bio && <p className="text-pretty text-xs mt-2">{bio}</p>}
 
           <LinksSocial />
 
-          <div className="min-h-[60%]"></div>
-
-          <h3 className="flex gap-2 items-center font-semibold">
-            Linktree clone <Palmtree className="size-4" />
-          </h3>
+          <div className="flex items-center justify-center gap-1 font-bold py-1.5 px-3 bg-white rounded-full shadow-md">
+            <TreePalm className="size-4" />
+            <p className="text-xs">Linktree clone</p>
+          </div>
         </div>
       </div>
     </div>
