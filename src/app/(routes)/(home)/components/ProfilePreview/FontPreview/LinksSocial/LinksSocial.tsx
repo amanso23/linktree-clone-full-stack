15e33d6dfeb06db5d5229c3bea723ac4fe575c34
Adrian <adrianmanso80@gmail.com>
2025-03-15 15:19:05 +0000

    Enhance LinksSocial component by adding minimum height for better layout consistency

--- src/app/(routes)/(home)/components/ProfilePreview/FontPreview/LinksSocial/LinksSocial.tsx
+++ src/app/(routes)/(home)/components/ProfilePreview/FontPreview/LinksSocial/LinksSocial.tsx
@@ -7,7 +7,7 @@ export function LinksSocial() {
   if (!links) return null;
 
   return (
-    <div className="flex flex-col text-center gap-2 mt-6 w-full">
+    <div className="flex flex-col text-center gap-2 mt-6 w-full min-h-[60%]">
       {links.map(({ name, link }) => (
         <a key={name} href={link || ""} title={`Link to my ${name}`}>
           <p className="px-1 py-1.5 w-full rounded-full bg-white/65 transition-all duration-300 hover:bg-transparent border border-black font-semibold">
