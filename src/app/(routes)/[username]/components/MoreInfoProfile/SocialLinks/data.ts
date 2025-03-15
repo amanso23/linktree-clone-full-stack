63953a24fcc6097ec659b693beb7b648c90ec8c4
Adrian <adrianmanso80@gmail.com>
2025-03-15 14:48:16 +0000

    Add dataLinks for social sharing in MoreInfoProfile component

---
+++ src/app/(routes)/[username]/components/MoreInfoProfile/SocialLinks/data.ts
@@ -0,0 +1,26 @@
+export const dataLinks = [
+  {
+    id: 1,
+    icon: "/social-networks/devicon--facebook.svg",
+    link: "https://www.facebook.com/sharer.php?u=https://linktreeclone.com",
+    social: "Facebook",
+  },
+  {
+    id: 2,
+    icon: "/social-networks/devicon--linkedin.svg",
+    link: "https://www.linkedin.com/sharer.php?u=https://linktreeclone.com",
+    social: "Linkedin",
+  },
+  {
+    id: 3,
+    icon: "/social-networks/logos--whatsapp-icon.svg",
+    link: "https://www.whatsapp.com/sharer.php?u=https://linktreeclone.com",
+    social: "Whatsapp",
+  },
+  {
+    id: 4,
+    icon: "/social-networks/devicon--twitter.svg",
+    link: "https://www.facebook.com/sharer.php?u=https://linktreeclone.com",
+    social: "X",
+  },
+];
