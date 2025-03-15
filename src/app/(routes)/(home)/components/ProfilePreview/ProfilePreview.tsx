f33b2b02d369198af7373e6b93576d4d3abad2be
Adrian <adrianmanso80@gmail.com>
2025-03-15 14:58:37 +0000

    Refactor ProfilePreview component to simplify structure and improve layout

--- src/app/(routes)/(home)/components/ProfilePreview/ProfilePreview.tsx
+++ src/app/(routes)/(home)/components/ProfilePreview/ProfilePreview.tsx
@@ -1,27 +1,11 @@
-import { useUserInfo } from "@/hooks/useUser";
-import Image from "next/image";
 import { Lock } from "lucide-react";
-import { ButtonsHeader } from "./ButtonsHeader";
-import { ButtonCopy } from "./ButtonCopy";
 import { FontPreview } from "./FontPreview";
 export function ProfilePreview() {
-  const { user, links } = useUserInfo();
-
-  if (!user) return null;
-
-  const { avatarUrl, backgroundImage, bio, name, username } = user;
-
   return (
-    <div className="flex flex-col gap-6 h-full w-full items-center text-center">
-      <ButtonsHeader />
-      <ButtonCopy />
-
+    <div className="flex flex-col gap-4 h-full w-full items-center text-center">
+      <div className="flex items-center gap-1"></div>
       <FontPreview />
-
-      <div className="flex items-center gap-2">
-        <p className="cursor-pointer">Hide linktree logo</p>
-        <Lock className="size-4" />
-      </div>
+      <p className="text-sm font-semibold">Profile preview</p>
     </div>
   );
 }
