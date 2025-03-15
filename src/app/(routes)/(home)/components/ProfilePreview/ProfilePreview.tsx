92689c97e036f13de78d426b495e3a478df73f99
Adrian <adrianmanso80@gmail.com>
2025-03-15 15:18:55 +0000

    Enhance ProfilePreview component by adding Eye icon for visual emphasis

--- src/app/(routes)/(home)/components/ProfilePreview/ProfilePreview.tsx
+++ src/app/(routes)/(home)/components/ProfilePreview/ProfilePreview.tsx
@@ -1,11 +1,14 @@
-import { Lock } from "lucide-react";
+import { Eye } from "lucide-react";
 import { FontPreview } from "./FontPreview";
 export function ProfilePreview() {
   return (
     <div className="flex flex-col gap-4 h-full w-full items-center text-center">
       <div className="flex items-center gap-1"></div>
       <FontPreview />
-      <p className="text-sm font-semibold">Profile preview</p>
+      <div className="flex items-center gap-1">
+        <p className="text-sm font-semibold">Profile preview</p>
+        <Eye className="size-5" />
+      </div>
     </div>
   );
 }
