0669947ab112c78cd0d95520fab0a7f5fe02413a
Adrian <adrianmanso80@gmail.com>
2025-03-16 09:14:38 +0000

    Update social media sharing links to remove trailing slashes for consistency

--- src/app/(routes)/[username]/components/MoreInfoProfile/SocialLinks/data.ts
+++ src/app/(routes)/[username]/components/MoreInfoProfile/SocialLinks/data.ts
@@ -2,25 +2,25 @@ export const dataLinks = [
   {
     id: 1,
     icon: "/social-networks/devicon--facebook.svg",
-    link: "https://www.facebook.com/sharer.php?u=https://linktreeclone.com/",
+    link: "https://www.facebook.com/sharer.php?u=https://linktreeclone.com",
     social: "Facebook",
   },
   {
     id: 2,
     icon: "/social-networks/devicon--linkedin.svg",
-    link: "https://www.linkedin.com/sharing/share-offside/?url=https://linktreeclone.com/",
+    link: "https://www.linkedin.com/sharing/share-offside/?url=https://linktreeclone.com",
     social: "Linkedin",
   },
   {
     id: 3,
     icon: "/social-networks/logos--whatsapp-icon.svg",
-    link: "https://wa.me/?text=Check%20out%20this%20Linktree!%20-%20https://linktreeclone.com/",
+    link: "https://wa.me/?text=Check%20out%20this%20Linktree!%20-%20https://linktreeclone.com",
     social: "Whatsapp",
   },
   {
     id: 4,
     icon: "/social-networks/devicon--twitter.svg",
-    link: "https://x.com/intent/tweet?text=Check%20out%20this%20Linktree!%20-%20https://linktreeclone.com/",
+    link: "https://x.com/intent/tweet?text=Check%20out%20this%20Linktree!%20-%20https://linktreeclone.com",
     social: "X",
   },
 ];
